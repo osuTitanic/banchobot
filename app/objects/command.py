4c77464815f3b4262b1f1d8026f5ca3c36d4827b
Lekuru <contact@lekuru.xyz>
2023-09-17 20:58:02 +0200

    Add `has_permission` to command

--- app/objects/command.py
+++ app/objects/command.py
@@ -1,9 +1,20 @@
 
 from typing import List, Callable
 from dataclasses import dataclass
+from discord import Member
 
 @dataclass
 class Command:
     function: Callable
     triggers: List[str]
     roles: List[str]
+
+    def has_permission(self, member: Member) -> bool:
+        """Check if member has permission to execute this command"""
+        member_roles = [role.name for role in member.roles]
+
+        for role in self.roles:
+            if role in member_roles:
+                return True
+
+        return False
