86ee3a2a794578109490f458e02948aa5a3eaee7
Lekuru <contact@lekuru.xyz>
2023-09-17 20:48:37 +0200

    Implement command roles

--- app/bot.py
+++ app/bot.py
@@ -18,7 +18,7 @@ class BanchoBot(discord.Client):
         trigger, *args = message.content.strip()[1:].split()
 
         app.session.logger.info(
-            f'[{message.author}] -> !{trigger}: {args}'
+            f'[{message.author}] -> {config.BOT_PREFIX}{trigger}: {args}'
         )
 
         if not (command := app.session.commands.get(trigger)):
@@ -27,6 +27,24 @@ class BanchoBot(discord.Client):
                 mention_author=True
             )
             return
+
+        if command.roles:
+            # Command requires permissions
+            author_roles = [role.name for role in message.author.roles]
+
+            for role in command.roles:
+                if role in author_roles:
+                    break
+            else:
+                # User doesn't have any of the required roles
+                app.session.logger.warning(
+                    f"[{message.author}] -> Tried to execute command {config.BOT_PREFIX}{trigger} but doesn't have the role for it"
+                )
+                await message.channel.send(
+                    'Your are not permitted to use that command.',
+                    mention_author=True
+                )
+                return
 
         # Try to execute command
         await command.function(
