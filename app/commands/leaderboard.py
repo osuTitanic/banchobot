39090c32bdc9dac012d4a501e01d1340965c331e
Levi <contact@lekuru.xyz>
2023-09-24 14:08:40 +0200

    Cleanup

--- app/commands/leaderboard.py
+++ app/commands/leaderboard.py
@@ -1,10 +1,12 @@
+
 from app.common.database.repositories import users
 from app.common.cache.leaderboards import top_players
 from app.objects import Context
+
 from discord import Embed
 from discord import Color
-import app
 
+import app
 
 @app.session.commands.register(["leaderboard", "lb"])
 async def leaderboard(context: Context):
@@ -12,11 +14,13 @@ async def leaderboard(context: Context):
     if not (user := users.fetch_by_discord_id(context.message.author.id)):
         await context.message.channel.send("You don't have an account linked!")
         return
+
     mode = user.preferred_mode
     modes = {"std": 0, "taiko": 1, "ctb": 2, "mania": 3}
     modes_reversed = {0: "Standard", 1: "Taiko", 2: "Catch the beat", 3: "Mania"}
     type = "pp"
-    if len(context.args):
+
+    if context.args:
         for arg in context.args:
             if arg in modes:
                 mode = modes[arg]
@@ -27,9 +31,11 @@ async def leaderboard(context: Context):
                     f"Wrong mode! Available modes: {', '.join(modes.keys())}"
                 )
                 return
+
     lb = top_players(
         mode=mode, range=10, type="performance" if type == "pp" else "rscore"
     )
+
     value_name = "pp" if type == "pp" else " ranked score"
     str = "```"
     position = 1
