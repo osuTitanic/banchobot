b6aed2c7fe050dd6402d149716cfed4e45610e0e
Lekuru <contact@lekuru.xyz>
2023-10-13 16:17:28 +0200

    Add `View Replay` button

--- app/commands/recent.py
+++ app/commands/recent.py
@@ -1,13 +1,43 @@
+
 from app.common.database.repositories import users, scores
-from app.common.database.objects import DBBeatmapset
+from app.common.database.objects import DBScore
 from app.common.constants import Mods
 from app.objects import Context
+from typing import Optional
+
+from discord.ui import View, Button
+from discord import ButtonStyle
+from discord import Interaction
 from discord import Embed
 from discord import Color
 
+import discord
 import config
 import app
+import io
 
+
+class ViewReplayButton(View):
+    def __init__(self, score: DBScore, *, timeout: Optional[float] = 250):
+        super().__init__(timeout=timeout)
+        self.score = score
+
+    @discord.ui.button(label='View Replay')
+    async def view_replay(self, interaction: Interaction, button: Button):
+        async with interaction.channel.typing():
+            replay = app.session.storage.get_full_replay(self.score.id)
+
+            if not replay:
+                await interaction.response.send_message(
+                    'Replay could not be found.'
+                )
+                return
+
+            button.disabled = True
+
+            await interaction.response.send_message(
+                file=discord.File(io.BytesIO(replay), filename=f'{self.score.id}.osr')
+            )
 
 @app.session.commands.register(["recent", "last"])
 async def recent(context: Context):
@@ -50,7 +80,12 @@ async def recent(context: Context):
         rank = f"F ({int((score.failtime/1000)/score.beatmap.total_length*100)}%)"
 
     embed.description = f"{rank} {max_combo}/{score.beatmap.max_combo} {accuracy*100:.2f}% [{n300}/{n100}/{n50}/{nmiss}] {pp:.2f}pp {nscore:,}"
+    replay = None
+
+    if app.session.storage.get_replay(score.id):
+        replay = ViewReplayButton(score)
 
     await context.message.channel.send(
-        embed=embed, reference=context.message, mention_author=True
+        embed=embed, reference=context.message, mention_author=True,
+        view=replay
     )
