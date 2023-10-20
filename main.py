fd41c56f4139a3b35f2d75b3068961b83868f604
Lekuru <contact@lekuru.xyz>
2023-10-20 15:49:25 +0200

    Add file logging

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
+from app.common.logging import Console, File
+
 import logging
 import utils
 import app
 
 logging.basicConfig(
     level=logging.INFO,
-    format='[%(asctime)s] - <%(name)s> %(levelname)s: %(message)s'
+    format='[%(asctime)s] - <%(name)s> %(levelname)s: %(message)s',
+    handlers=[Console, File]
 )
 
 def main():
