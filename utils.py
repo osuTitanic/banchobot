1b1a66e6af48f1f6f914f7d3787790327c687c78
Lekuru <contact@lekuru.xyz>
2023-11-11 15:56:02 +0100

    Remove `compute_score_checksum` and `get_ticks`

--- utils.py
+++ utils.py
@@ -16,29 +16,6 @@ def setup():
         os.makedirs(f'{config.DATA_PATH}/replays', exist_ok=True)
         os.makedirs(f'{config.DATA_PATH}/avatars', exist_ok=True)
 
-def compute_score_checksum(score: DBScore) -> str:
-    return hashlib.md5(
-        '{}p{}o{}o{}t{}a{}r{}e{}y{}o{}u{}{}{}'.format(
-            (score.n100 + score.n300),
-            score.n50,
-            score.nGeki,
-            score.nKatu,
-            score.nMiss,
-            score.beatmap.md5,
-            score.max_combo,
-            score.perfect,
-            score.user.name,
-            score.total_score,
-            score.grade,
-            score.mods,
-            (not score.failtime) # (passed)
-        ).encode()
-    ).hexdigest()
-
-def get_ticks(dt) -> int:
-    dt = dt.replace(tzinfo=None)
-    return int((dt - datetime(1, 1, 1)).total_seconds() * 10000000)
-
 def get_beatmap_filename(id: int) -> str:
     response = requests.head(f'https://old.ppy.sh/osu/{id}')
 
