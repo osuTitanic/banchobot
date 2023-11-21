7eba15c34af5aa1554a2daa4cbb164cf8eb382aa
Lekuru <contact@lekuru.xyz>
2023-11-21 20:06:50 +0100

    Fix value names

--- app/commands/leaderboard.py
+++ app/commands/leaderboard.py
@@ -19,7 +19,7 @@ async def leaderboard(context: Context):
 
     mode = user.preferred_mode
     modes = {"std": 0, "taiko": 1, "ctb": 2, "mania": 3}
-    modes_reversed = {0: "Standard", 1: "Taiko", 2: "Catch the beat", 3: "Mania"}
+    modes_reversed = {0: "Standard", 1: "Taiko", 2: "Catch The Beat", 3: "Mania"}
     type = "pp"
     
     if context.args:
@@ -35,7 +35,7 @@ async def leaderboard(context: Context):
                 return
 
     order_type = {'pp': 'performance', 'score': 'rscore', 'total_score': 'tscore', 'ppv1': 'ppv1'}[type]
-    value_name = {'pp': 'pp', 'score': 'ranked score', 'total_score': 'total score', 'ppv1': 'ppv1'}[type]
+    value_name = {'pp': 'pp', 'score': '', 'total_score': '', 'ppv1': 'pp'}[type]
 
     lb = top_players(
         mode=mode, range=10, type=order_type
