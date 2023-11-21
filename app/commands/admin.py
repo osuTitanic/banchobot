ffd23b63379c71ed5e364c7c456bf7cea99f21c6
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-11-21 15:54:05 +0000

    Fetch user by mention in !restrict/!rename command

--- app/commands/admin.py
+++ app/commands/admin.py
@@ -7,18 +7,31 @@ import app
 @app.session.commands.register(['restrict', 'unrestrict'], roles=['Admin'])
 async def restrict(context: Context):
     """<user_id> <reason> - (Un)Restrict user"""
-    if not context.args or not context.args[0].isnumeric():
+    if not context.args:
         await context.message.channel.send(
-            f'Invalid syntax: `!{context.command} <user_id> <reason>`',
+            f'Invalid syntax: `!{context.command} <user_id/mention> <reason>`',
             reference=context.message,
             mention_author=True
         )
         return
     
-    user_id = int(context.args[0])
-    reason = "No reason." if len(context.args) < 2 else " ".join(context.args[1:])
+    if context.args[0].isnumeric():
+        discord_id = None
+        user = users.fetch_by_id(int(context.args[0]))
+    elif context.args[0].startswith("<@"):
+        discord_id = int(context.args[0][2:-1])
+        user = users.fetch_by_discord_id(discord_id)
+    else:
+        await context.message.channel.send(
+            f'Invalid syntax: `!{context.command} <user_id/mention> <reason>`',
+            reference=context.message,
+            mention_author=True
+        )
+        return
 
-    if (user := users.fetch_by_id(user_id)) is None:
+    reason = "No reason." if len(context.args) < 3 else " ".join(context.args[2:])
+
+    if not user:
         await context.message.channel.send(
             f'User not found!',
             reference=context.message,
@@ -34,10 +47,10 @@ async def restrict(context: Context):
                 mention_author=True
             )
         else:
-            users.update(user_id, {'restricted': True})
+            users.update(user.id, {'restricted': True})
             app.session.events.submit(
                 'restrict',
-                user_id=user_id,
+                user_id=user.id,
                 reason=reason
             )
             await context.message.channel.send(
@@ -53,7 +66,7 @@ async def restrict(context: Context):
                 mention_author=True
             )
         else:
-            users.update(user_id, {'restricted': False})
+            users.update(user.id, {'restricted': False})
             await context.message.channel.send(
                 f'User unrestricted.',
                 reference=context.message,
@@ -63,7 +76,7 @@ async def restrict(context: Context):
 @app.session.commands.register(['rename'], roles=['Admin'])
 async def rename(context: Context):
     """<user_id> <new_name> - rename user"""
-    if len(context.args) < 2 or not context.args[0].isnumeric():
+    if len(context.args) < 2:
         await context.message.channel.send(
             f'Invalid syntax: `!{context.command} <user_id> <new_name>`',
             reference=context.message,
@@ -71,11 +84,22 @@ async def rename(context: Context):
         )
         return
 
-    user_id = int(context.args[0])
     username = " ".join(context.args[1:])
     
-    if users.fetch_by_id(user_id):
-        users.update(user_id, {'name': username})
+    if context.args[0].isnumeric():
+        user = users.fetch_by_id(int(context.args[0]))
+    elif context.args[0].startswith("<@"):
+        user = users.fetch_by_discord_id(int(context.args[0][2:-1]))
+    else:
+        await context.message.channel.send(
+            f'Invalid syntax: `!{context.command} <user_id/mention> <new_name>`',
+            reference=context.message,
+            mention_author=True
+        )
+        return
+    
+    if user:
+        users.update(user.id, {'name': username})
         await context.message.channel.send(
             'User renamed.',
             reference=context.message,
