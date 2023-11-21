f8a11cae0ed0599c94f8575a5ccf2db211a92279
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-11-21 19:23:10 +0000

    separate restrict/unrestrict command

--- app/commands/admin.py
+++ app/commands/admin.py
@@ -4,9 +4,9 @@ from app.objects import Context
 
 import app
 
-@app.session.commands.register(['restrict', 'unrestrict'], roles=['Admin'])
+@app.session.commands.register(['restrict'], roles=['Admin'])
 async def restrict(context: Context):
-    """<user_id> <reason> - (Un)Restrict user"""
+    """<user_id> <reason> - Restrict user"""
     if not context.args:
         await context.message.channel.send(
             f'Invalid syntax: `!{context.command} <user_id/mention> <reason>`',
@@ -39,43 +39,76 @@ async def restrict(context: Context):
         )
         return
 
-    if context.command == "restrict":
-        if user.restricted:
-            await context.message.channel.send(
-                f'User is already restricted!',
-                reference=context.message,
-                mention_author=True
-            )
-        else:
-            users.update(user.id, {'restricted': True})
-            app.session.events.submit(
-                'restrict',
-                user_id=user.id,
-                reason=reason
-            )
-            await context.message.channel.send(
-                f'User restricted.',
-                reference=context.message,
-                mention_author=True
-            )
+    if user.restricted:
+        await context.message.channel.send(
+            f'User is already restricted!',
+            reference=context.message,
+            mention_author=True
+        )
     else:
-        if not user.restricted:
-            await context.message.channel.send(
-                f'User is already unrestricted!',
-                reference=context.message,
-                mention_author=True
-            )
-        else:
-            users.update(user.id, {'restricted': False})
-            await context.message.channel.send(
-                f'User unrestricted.',
-                reference=context.message,
-                mention_author=True
-            )
+        users.update(user.id, {'restricted': True})
+        app.session.events.submit(
+            'restrict',
+            user_id=user.id,
+            reason=reason
+        )
+        await context.message.channel.send(
+            f'User restricted.',
+            reference=context.message,
+            mention_author=True
+        )
+            
+
+@app.session.commands.register(['unrestrict'], roles=['Admin'])
+async def unrestrict(context: Context):
+    """<user_id> - Unrestrict user"""
+    if not context.args:
+        await context.message.channel.send(
+            f'Invalid syntax: `!{context.command} <user_id/mention>`',
+            reference=context.message,
+            mention_author=True
+        )
+        return
+    
+    if context.args[0].isnumeric():
+        discord_id = None
+        user = users.fetch_by_id(int(context.args[0]))
+    elif context.args[0].startswith("<@"):
+        discord_id = int(context.args[0][2:-1])
+        user = users.fetch_by_discord_id(discord_id)
+    else:
+        await context.message.channel.send(
+            f'Invalid syntax: `!{context.command} <user_id/mention> <reason>`',
+            reference=context.message,
+            mention_author=True
+        )
+        return
+
+    if not user:
+        await context.message.channel.send(
+            f'User not found!',
+            reference=context.message,
+            mention_author=True
+        )
+        return
+
+    if not user.restricted:
+        await context.message.channel.send(
+            f'User is already unrestricted!',
+            reference=context.message,
+            mention_author=True
+        )
+    else:
+        users.update(user.id, {'restricted': False})
+        await context.message.channel.send(
+            f'User unrestricted.',
+            reference=context.message,
+            mention_author=True
+        )
 
 @app.session.commands.register(['rename'], roles=['Admin'])
 async def rename(context: Context):
-    """<user_id> <new_name> - rename user"""
+    """<user_id> <new_name> - Rename user"""
     if len(context.args) < 2:
         await context.message.channel.send(
             f'Invalid syntax: `!{context.command} <user_id> <new_name>`',
