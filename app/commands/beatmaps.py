e6ed28be2f6e6aadd926ba1acb1148c7ce1c42f9
Lekuru <contact@lekuru.xyz>
2023-11-23 19:47:42 +0100

    Add `fixset` command

--- app/commands/beatmaps.py
+++ app/commands/beatmaps.py
@@ -3,6 +3,7 @@ from app.common.database.repositories import beatmapsets, beatmaps
 from app.objects import Context
 from ossapi import OssapiV1
 
+import hashlib
 import config
 import utils
 import app
@@ -96,6 +97,80 @@ async def add_beatmapset(context: Context):
 
         await context.message.channel.send(
             f'[Beatmapset was created.](http://osu.{config.DOMAIN_NAME}/s/{db_set.id})',
+            reference=context.message,
+            mention_author=True
+        )
+
+@app.session.commands.register(['fixset'], roles=['Admin'])
+async def fix_beatmapset(context: Context):
+    """<set_id> - Fix the beatmap files for a beatmapset"""
+
+    if not context.args or not context.args[0].isnumeric():
+        await context.message.channel.send(
+            f'Invalid syntax: `!{context.command} <set_id>`',
+            reference=context.message,
+            mention_author=True
+        )
+        return
+
+    set_id = int(context.args[0])
+
+    async with context.message.channel.typing():
+        if not (beatmapset := beatmapsets.fetch_one(set_id)):
+            await context.message.channel.send(
+                'This beatmapset does not exist!',
+                reference=context.message,
+                mention_author=True
+            )
+            return
+
+        for beatmap in beatmapset.beatmaps:
+            beatmap_file = app.session.storage.get_beatmap(beatmap.id)
+            beatmap_dict = utils.parse_beatmap_file(beatmap_file.decode())
+            beatmap_updates = {}
+
+            difficulty_attributes = {
+                'OverallDifficulty': 'od',
+                'ApproachRate': 'ar',
+                'HPDrainRate': 'hp',
+                'CircleSize': 'cs'
+            }
+
+            for key, short_key in difficulty_attributes.items():
+                if key not in beatmap_dict['Difficulty']:
+                    continue
+
+                value = beatmap_dict['Difficulty'][key]
+
+                if isinstance(value, int):
+                    continue
+
+                # Update value
+                beatmap_updates[short_key] = round(value) # Database
+                beatmap_dict['Difficulty'][key] = round(value) # File
+
+            if not beatmap_updates:
+                continue
+
+            # Get new file
+            content = utils.get_beatmap_file(beatmap_dict)
+
+            # Upload to storage
+            app.session.storage.upload_beatmap_file(beatmap.id, content)
+
+            # Update beatmap hash
+            beatmap_hash = hashlib.md5(content).hexdigest()
+            beatmap_updates['md5'] = beatmap_hash
+
+            # Update database
+            beatmaps.update(beatmap.id, beatmap_updates)
+
+            await context.message.channel.send(
+                f'Updated "[{beatmap.version}](http://osu.{config.DOMAIN_NAME}/b/{beatmap.id})"'
+            )
+
+        await context.message.channel.send(
+            'Done.',
             reference=context.message,
             mention_author=True
         )
