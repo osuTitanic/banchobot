13cdc0f0c1054125fc2e6989588c20110e8cb093
Lekuru <contact@lekuru.xyz>
2024-02-24 22:06:31 +0100

    Fix session management for `!top`

--- app/commands/top.py
+++ app/commands/top.py
@@ -12,58 +12,65 @@ import app
 @app.session.commands.register(["top"])
 async def top(context: Context):
     """<std/taiko/ctb/mania> (username) - Display the top plays of yourself or another player"""
-
-    if not (user := users.fetch_by_discord_id(context.message.author.id)):
-        await context.message.channel.send("You don't have an account linked!")
-        return
-
-    mode = user.preferred_mode
-    modes = {"std": 0, "taiko": 1, "ctb": 2, "mania": 3}
-
-    if len(context.args):
-        if context.args[0] in modes:
-            mode = modes[context.args[0]]
-        else:
-            await context.message.reply(
-                f"Wrong mode! Available modes: {', '.join(modes.keys())}"
-            )
+    with app.session.database.managed_session() as session:
+        if not (user := users.fetch_by_discord_id(context.message.author.id, session=session)):
+            await context.message.channel.send("You don't have an account linked!")
             return
-    
-    if len(context.args) > 1:
-        if not (user := users.fetch_by_name_extended(context.args[1])):
-            await context.message.channel.send("User not found!")
-            return
-    
-    if not (user_scores := scores.fetch_top_scores(user_id=user.id, mode=mode, limit=10)):
-        await context.message.reply(f"No scores found for user {user.name}.")
-        return
 
-    str_builder = ""
-    position = 1
+        mode = user.preferred_mode
+        modes = {"std": 0, "taiko": 1, "ctb": 2, "mania": 3}
 
-    for score in user_scores:
-        rank = score.grade
-        max_combo = score.max_combo
-        accuracy = score.acc
-        n300 = score.n300
-        n100 = score.n100
-        n50 = score.n50
-        nmiss = score.nMiss
-        pp = score.pp
-        mods = Mods(score.mods).short
-        beatmap_title = (
-            f"{score.beatmap.beatmapset.title} [{score.beatmap.version}] +{mods}"
+        if len(context.args):
+            if context.args[0] in modes:
+                mode = modes[context.args[0]]
+            else:
+                await context.message.reply(
+                    f"Wrong mode! Available modes: {', '.join(modes.keys())}"
+                )
+                return
+
+        if len(context.args) > 1:
+            if not (user := users.fetch_by_name_extended(context.args[1], session=session)):
+                await context.message.channel.send("User not found!")
+                return
+
+        user_scores = scores.fetch_top_scores(
+            user_id=user.id,
+            mode=mode,
+            limit=10,
+            session=session
         )
-        str_builder += f"{position}. {beatmap_title}\n"
-        str_builder += f"   {rank} {max_combo}/{score.beatmap.max_combo} {accuracy*100:.2f}% [{n300}/{n100}/{n50}/{nmiss}] {pp:.2f}pp\n"
-        position += 1
 
-    embed = Embed(
-        title=f"Top plays for {user.name}",
-        url=f"http://osu.{config.DOMAIN_NAME}/u/{user.id}#leader",
-        color=Color.blue(),
-    )
+        if not user_scores:
+            await context.message.reply(f"No scores found for user {user.name}.")
+            return
 
-    embed.set_thumbnail(url=f"http://osu.{config.DOMAIN_NAME}/a/{user.id}?h=50")
-    embed.description = str_builder
-    await context.message.channel.send(embed=embed)
+        str_builder = ""
+        position = 1
+
+        for score in user_scores:
+            rank = score.grade
+            max_combo = score.max_combo
+            accuracy = score.acc
+            n300 = score.n300
+            n100 = score.n100
+            n50 = score.n50
+            nmiss = score.nMiss
+            pp = score.pp
+            mods = Mods(score.mods).short
+            beatmap_title = (
+                f"{score.beatmap.beatmapset.title} [{score.beatmap.version}] +{mods}"
+            )
+            str_builder += f"{position}. {beatmap_title}\n"
+            str_builder += f"   {rank} {max_combo}/{score.beatmap.max_combo} {accuracy*100:.2f}% [{n300}/{n100}/{n50}/{nmiss}] {pp:.2f}pp\n"
+            position += 1
+
+        embed = Embed(
+            title=f"Top plays for {user.name}",
+            url=f"http://osu.{config.DOMAIN_NAME}/u/{user.id}#leader",
+            color=Color.blue(),
+        )
+
+        embed.set_thumbnail(url=f"http://osu.{config.DOMAIN_NAME}/a/{user.id}?h=50")
+        embed.description = str_builder
+        await context.message.channel.send(embed=embed)
