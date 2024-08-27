c18cbd84699b3cad73c79cf049837aebe1c0dc72
ascenttree <tree@ascension.wtf>
2024-08-28 06:15:09 +1000

    remove line instead of commenting

--- utils.py
+++ utils.py
@@ -39,9 +39,6 @@ def parse_beatmap_file(content: str) -> Dict[str, dict]:
     current_section = None
 
     for line in content.splitlines():
-        # this line breaks storyboards
-        # line = line.strip()
-
         if (line.startswith('[') and line.endswith(']')):
             # New section
             current_section = line.removeprefix('[').removesuffix(']')
