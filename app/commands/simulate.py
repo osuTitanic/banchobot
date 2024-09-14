670c4280765be40ff2891d9e9f6b410bede8a149
Lekuru <contact@lekuru.xyz>
2024-09-14 13:23:15 +0200

    Cleanup simulate command

--- app/commands/simulate.py
+++ app/commands/simulate.py
@@ -8,36 +8,48 @@ import app
 @app.session.commands.register(["simulate", "pp"])
 async def simulate(context: Context):
     """Simulate pp for a beatmap"""
-    possible_args = ('id', 'acc', 'mods', 'mode', 'combo', 'n300', 'n100', 'n50', 'geki', 'katu', 'miss')
     msg = context.message.content.split(" ")[1:]
     args = {}
+
+    possible_args = (
+        'id', 'acc', 'mods', 'mode',
+        'combo', 'n300', 'n100',
+        'n50', 'geki', 'katu', 'miss'
+    )
 
     if len(msg) % 2 != 0:
         await context.message.reply(f"Wrong arguments! \nList of available arguments: {', '.join(possible_args)}")
         return
 
-    for x in range(0, len(msg), 2):
-        if not msg[x].startswith("-"):
+    for index in range(0, len(msg), 2):
+        if not msg[index].startswith("-"):
             await context.message.reply(f"Arguments should start with \"-\"!")
             return
 
-        if msg[x][1:] not in possible_args:
-            await context.message.reply(f"Unknown argument {msg[x]}! \nList of available arguments: {', '.join(possible_args)}")
+        argument_name = msg[index][1:]
+
+        if argument_name not in possible_args:
+            await context.message.reply(f"Unknown argument {msg[index]}! \nList of available arguments: {', '.join(possible_args)}")
             return
 
-        if msg[x][1:] == "mods":
-            args['mods'] = Mods.from_string(msg[x+1])
-        elif msg[x][1:] == "acc":
+        if argument_name == "mods":
+            args['mods'] = Mods.from_string(msg[index+1])
+            continue
+
+        elif argument_name == "acc":
             try:
-                args['acc'] = float(msg[x+1])
+                acc = msg[index+1].removesuffix("%")
+                args['acc'] = float(acc)
+                continue
             except ValueError:
                 await context.message.reply(f"Arguments must be numeric!")
                 return
-        else:
-            if not msg[x+1].isnumeric():
-                await context.message.reply(f"Arguments must be numeric!")
-                return
-            args[msg[x][1:]] = int(msg[x+1])
+
+        elif not msg[index+1].isnumeric():
+            await context.message.reply(f"Arguments must be numeric!")
+            return
+
+        args[msg[index][1:]] = int(msg[index+1])
 
     if not 'id' in args:
         await context.message.reply(f"Provide beatmap id!")
@@ -47,27 +59,28 @@ async def simulate(context: Context):
         await context.message.reply(f"Can't find beatmap!")
         return
 
-    calc = Calculator(mode=args['mode'] if 'mode' in args else 0)
-    if 'acc' in args:
-        calc.set_acc(args['acc'])
-    if 'mods' in args:
-        calc.set_mods(args['mods'])
-    if 'combo' in args:
-        calc.set_combo(args['combo'])
-    if 'n300' in args:
-        calc.set_n300(args['n300'])
-    if 'n100' in args:
-        calc.set_n100(args['n100'])
-    if 'n50' in args:
-        calc.set_n50(args['n50'])
-    if 'katu' in args:
-        calc.set_n_katu(args['katu'])
-    if 'geki' in args:
-        calc.set_n_geki(args['geki'])
-    if 'miss' in args:
-        calc.set_n_misses(args['miss'])
-        
+    calc = Calculator(
+        mode=args['mode']
+        if 'mode' in args else 0
+    )
+
+    functions = {
+        'acc': calc.set_acc,
+        'mods': calc.set_mods,
+        'combo': calc.set_combo,
+        'n300': calc.set_n300,
+        'n100': calc.set_n100,
+        'n50': calc.set_n50,
+        'katu': calc.set_n_katu,
+        'geki': calc.set_n_geki,
+        'miss': calc.set_n_misses
+    }
+
+    for key, value in args.items():
+        if key in functions:
+            functions[key](value)
+
     bm = Beatmap(bytes=beatmap_file)
-    
+
     result = calc.performance(bm)
     await context.message.reply(f"PP: {result.pp:.2f}")
