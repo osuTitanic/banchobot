8d3bfc6216e33356744e01d9e3e3388302c55a11
Lekuru <contact@lekuru.xyz>
2025-09-21 19:42:58 +0200

    Remove `/addset` from todo

--- app/extensions/beatmaps.py
+++ app/extensions/beatmaps.py
@@ -1,13 +1,12 @@
 
 # TODO:
-# /addset - Add a beatmapset from bancho to the local database
 # /downloadset - Download a beatmapset from bancho to local storage
 # /deleteset - Delete a beatmapset from the local database
 # /deletemap - Delete a single beatmap from the local database
 # /modset - Modify a beatmapset's status
 # /moddiff - Modify a single beatmap's status
-# /fixhash - Update the .osu file hashes of a beatmapset
 # /uploadmap - Upload a single beatmap to local storage
+# /fixhash - Update the .osu file hashes of a beatmapset
 
 from app.common.database.repositories import beatmapsets
 from app.common.database.objects import DBBeatmapset
