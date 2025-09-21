9a813a2015af827942749d5f8bb8f15d1f308ba4
Lekuru <contact@lekuru.xyz>
2025-09-21 18:10:09 +0200

    Add todo list for beatmap commands

---
+++ app/extensions/beatmaps.py
@@ -0,0 +1,9 @@
+
+# TODO:
+# /addset - Add a beatmapset from bancho to the local database
+# /downloadset - Download a beatmapset from bancho to local storage
+# /deleteset - Delete a beatmapset from the local database
+# /deletemap - Delete a single beatmap from the local database
+# /modset - Modify a beatmapset's status
+# /moddiff - Modify a single beatmap's status
+# /fixhash - Update the .osu file hashes of a beatmapset
